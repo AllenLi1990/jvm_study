f016c46223addf8ee187ce82191611b243fe7c8f
liyj4 <282154340@qq.com>
2014-04-11 00:22:35 +0800

    home change

---
+++ src/main/java/com/liyj4/jvm/chapter3/ReferenceCountingGC.java
@@ -0,0 +1,27 @@
+package com.liyj4.jvm.chapter3;
+
+/**
+ * Created by lyj on 2014/4/11.
+ */
+public class ReferenceCountingGC {
+
+    public Object instance = null;
+
+    private static final int _1MB = 1024 * 1024;
+
+    private byte[] bigSize = new byte[2 * _1MB];
+
+    public static void testGC(){
+        ReferenceCountingGC objA = new ReferenceCountingGC();
+        ReferenceCountingGC objB = new ReferenceCountingGC();
+
+        objA.instance = objB;
+        objB.instance = objA;
+
+        objA = null;
+        objB = null;
+
+        System.gc();
+    }
+
+}
